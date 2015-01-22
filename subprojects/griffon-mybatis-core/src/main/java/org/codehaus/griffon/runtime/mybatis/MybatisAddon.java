44e967b18a36b21c63dd4a976d6e4bd3af08da11
aalmiray <aalmiray@gmail.com>
2015-01-22 12:30:49 +0100

    Miscellaneous updates

--- subprojects/griffon-mybatis-core/src/main/java/org/codehaus/griffon/runtime/mybatis/MybatisAddon.java
+++ subprojects/griffon-mybatis-core/src/main/java/org/codehaus/griffon/runtime/mybatis/MybatisAddon.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ public class MybatisAddon extends AbstractGriffonAddon {
     @Inject
     private MybatisFactory mybatisFactory;
 
-    public void onBootstrapEnd(@Nonnull GriffonApplication application) {
+    public void onStartupStart(@Nonnull GriffonApplication application) {
         for (String sessionFactoryName : mybatisFactory.getSessionFactoryNames()) {
             Map<String, Object> config = mybatisFactory.getConfigurationFor(sessionFactoryName);
             if (getConfigValueAsBoolean(config, "connect_on_startup", false)) {
